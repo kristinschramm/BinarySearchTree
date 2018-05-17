e9a572041ca2bc49bc7d882e8617149291d7412b
kristinschramm <kristin.schramm@gmail.com>
2018-05-17 09:25:45 -0500

    commit binary search tree

---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Tree
+    {
+        List<string> directions = new List<string>();
+
+        public Node AddNode(Node node, int value)
+        {
+            if (node == null)
+            {
+                node = new Node();
+                node.value = value;
+               
+            }
+            else if (node.value == value)
+            {
+                return node;
+            }
+            else if (node.value> value)
+            {
+                node.left= AddNode(node.left, value);
+            }
+            else if (node.value < value)
+            {
+                node.right = AddNode(node.right, value);
+            }
+            return node;
+        }
+
+        public Node FindValue(Node node, int value)
+        {
+            if (node.value == value)
+            {
+                return node;
+            }
+            else if (node.value > value)
+            {
+                directions.Add("Left ");
+                FindValue(node.left, value);
+
+            }
+            else if (node.value < value)
+            {
+                directions.Add("Right ");
+                FindValue(node.right, value);
+            }
+
+            Console.WriteLine(directions);
+
+            return node;
+        }
+
+    }
+}
